acc71cb9868d20fef46f9b142794000122710f49
Tjebbe Hepkema <t.m.hepkema@uu.n>
2021-05-14 14:17:10 +0200

    set new paths

--- plantA.jl
+++ plantA.jl
@@ -1,16 +1,16 @@
-using Images
 using Pkg
 Pkg.activate(".")
 
+using Images
 using Revise
 using Random, ImageView
 
 
-includet("K3M.jl")
+includet("src/K3M.jl")
 
 
 # load image
-img = load("plant.jpg")
+img = load("img/plant.jpg")
 img_crop = img[1000:end-360,900:end-1100] # crop
 # img_r = imresize(img_crop, ratio=0.5) # resize
 # imgg1 = Gray.(img_r) # convert to grayscale
@@ -24,12 +24,12 @@ imgg1 = imgg1 .< 0.70; # threshold
 nr_iters = 50
 bimg = copy(imgg1);
 results,borders = K3M!(bimg,nr_iters);
-save("k3m.gif",results)
-save("k3m_b.gif",borders)
+save("output/k3m.gif",results)
+save("output/k3m_b.gif",borders)
 
 skelet = results[:,:,end];
 
 
 skelet_green = RGB.(0,skelet,0)
-save("skelet_green.jpg",skelet_green)
+save("output/skelet_green.jpg",skelet_green)
 
